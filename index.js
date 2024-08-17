4a1a38fa08e3b4dfc7a7115136427c739952b4cb
Eldridge Gomes <eldridgegomes03@gmail.com>
2024-08-17 23:47:44 +0530

    Updated topHeadlines api params

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const newsapi = new NewsAPI('API_KEY');
 
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-// newsapi.v2.topHeadlines({
-//   sources: 'bbc-news,the-verge',
-//   q: 'bitcoin',
-//   category: 'business',
-//   language: 'en',
-//   country: 'us'
-// }).then(response => {
-//   console.log(response);
-//   /*
-//     {
-//       status: "ok",
-//       articles: [...]
-//     }
-//   */
-// });
+newsapi.v2.topHeadlines({
+  q: 'trump',
+  category: 'politics',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
 
 
 // To query /v2/everything
