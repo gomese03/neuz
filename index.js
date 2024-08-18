d1ad20c800c2358fbbafa1ade24caddf649021fb
Eldridge Gomes <eldridgegomes03@gmail.com>
2024-08-18 09:02:27 +0530

    Added env files

--- index.js
+++ index.js
@@ -1,58 +1,8 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('API_KEY');
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  q: 'trump',
-  category: 'politics',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
+console.log(apiKey);
 
+// const NewsAPI = require('newsapi');
+// const newsapi = new NewsAPI('API_KEY');
 
-// To query /v2/everything
-// You must include at least one q, source, or domain
-// newsapi.v2.everything({
-//   q: 'bitcoin',
-//   sources: 'bbc-news,the-verge',
-//   domains: 'bbc.co.uk, techcrunch.com',
-//   from: '2017-12-01',
-//   to: '2017-12-12',
-//   language: 'en',
-//   sortBy: 'relevancy',
-//   page: 2
-// }).then(response => {
-//   console.log(response);
-//   /*
-//     {
-//       status: "ok",
-//       articles: [...]
-//     }
-//   */
-// });
-
-
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
-});
